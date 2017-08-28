95a90d53f151970bedf0a009ddaaf901c83f3732
Kentaroh Nagai <kentaroh_nagai@ricoh-japan.co.jp>
2017-08-28 10:20:20 +0900

    Add-SingletonSampleCode

---
+++ SingletonSample/SingletonSample/ConfirmSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SingletonSample
+{
+    public class ConfirmSingleton
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            Singleton obj1 = Singleton.GetInstance();
+            Singleton obj2 = Singleton.GetInstance();
+
+            if (obj1 == obj2)
+            {
+                Console.WriteLine("obj1とobj2は同じインスタンスです。");
+            }
+            else
+            {
+                Console.WriteLine("obj1とobj2は同じインスタンスではありません。");
+            }
+
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonSample/SingletonSample/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SingletonSample
+{
+    public class Singleton
+    {
+        private static Singleton _singleInstance = new Singleton();
+        private Singleton()
+        {
+            Console.WriteLine("インスタンスを生成しました。");
+        }
+
+        public static Singleton GetInstance()
+        {
+            return _singleInstance;
+        }
+    }
+}

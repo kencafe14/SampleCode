279aef29db743307de145a7cd354dd774cee9f3f
Kentaroh Nagai <kentaroh_nagai@ricoh-japan.co.jp>
2017-08-30 11:24:41 +0900

    Fix indentation

--- SingletonSample/SingletonSample/Singleton.cs
+++ SingletonSample/SingletonSample/Singleton.cs
@@ -5,6 +5,7 @@ namespace SingletonSample
     public class Singleton
     {
         private static Singleton _singleton = new Singleton();
+
         private Singleton()
         {
             Console.WriteLine("インスタンスを生成しました。");

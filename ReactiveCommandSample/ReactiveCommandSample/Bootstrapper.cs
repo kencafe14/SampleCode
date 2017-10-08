ea9d1e467ca9ac0c5bd93944cc363bff8a51266d
Keenag <keenag14@gamil.com>
2017-10-08 22:48:51 +0900

    AddReactiveCommandSamle

---
+++ ReactiveCommandSample/ReactiveCommandSample/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Prism.Autofac;
+using ReactiveCommandSample.Views;
+using System.Windows;
+
+namespace ReactiveCommandSample
+{
+    class Bootstrapper : AutofacBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ ReactiveCommandSample/ReactiveCommandSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using System.Reactive.Linq;
+
+namespace ReactiveCommandSample.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public ReactiveProperty<bool> IsActive { get; } = new ReactiveProperty<bool>(true);
+        public ReactiveCommand HogeCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            HogeCommand = IsActive.Select(x => x == true).ToReactiveCommand();
+            HogeCommand.Subscribe(OnClick);
+        }
+
+        public void OnClick()
+        {
+            //ボタンが押下された際の処理
+        }
+    }
+}
